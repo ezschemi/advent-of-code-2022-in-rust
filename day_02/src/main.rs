53c707d35b35b477a3f0af8996f7017d5e1bfdc7
Enrico Zschemisch <en@ruw.io>
2022-12-08 14:28:48 +0100

    added more Rust-like solution. Added solution with iterators.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,57 +1,179 @@
 use std::fs;
+use std::str::FromStr;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
-enum HandShape {
+enum Move {
     Rock,
     Paper,
     Scissors,
 }
 
+impl TryFrom<char> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'A' | 'X' => Ok(Self::Rock),
+            'B' | 'Y' => Ok(Self::Paper),
+            'C' | 'Z' => Ok(Self::Scissors),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move: {value:?}")),
+        }
+    }
+}
+
+impl Move {
+    fn inherent_points(self) -> usize {
+        match self {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        }
+    }
+
+    fn beats(self, opponents_play: Move) -> bool {
+        matches!(
+            (self, opponents_play),
+            (Move::Rock, Move::Scissors)
+                | (Move::Paper, Move::Rock)
+                | (Move::Scissors, Move::Paper)
+        )
+    }
+
+    fn outcome(self, opponents_play: Move) -> Outcome {
+        if self.beats(opponents_play) {
+            Outcome::Win
+        } else if opponents_play.beats(self) {
+            Outcome::Lose
+        } else {
+            Outcome::Draw
+        }
+    }
+}
+#[derive(Copy, Clone, Debug)]
 enum Outcome {
     Win,
     Lose,
     Draw,
 }
 
-#[derive(Debug)]
+impl Outcome {
+    fn inherent_points(self) -> usize {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Lose => 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 struct Round {
-    opponents_play: HandShape,
-    my_play: HandShape,
+    // these two are not used right now, but I want to keep them
+    #[allow(dead_code)]
+    opponents_play: Move,
+    #[allow(dead_code)]
+    my_play: Move,
+
     score: usize,
 }
 
-fn calculate_score(opponents_play: HandShape, my_play: HandShape) -> usize {
+impl FromStr for Round {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut chars = s.chars();
+
+        let (Some(opponents_play), Some(' '), Some(my_play), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got {s:?}"));
+        };
+
+        Ok(Self {
+            opponents_play: opponents_play.try_into()?,
+            my_play: my_play.try_into()?,
+            score: 0,
+        })
+    }
+}
+
+impl Round {
+    fn outcome(self) -> Outcome {
+        self.my_play.outcome(self.opponents_play)
+    }
+
+    fn my_score(self) -> usize {
+        self.my_play.inherent_points() + self.outcome().inherent_points()
+    }
+}
+
+fn more_rustlike() -> color_eyre::Result<()> {
+    // this collects all the rounds into the vector
+    // thus using the memory.
+    // BUT: All that's needed is the sum!
+    let rounds: Vec<Round> = include_str!("../input.txt")
+        .lines()
+        .map(|line| line.parse())
+        .collect::<Result<_, _>>()?;
+
+    let total_score: usize = rounds.iter().map(|round| round.my_score()).sum();
+    println!("total score: {}", total_score);
+
+    // with this imperative approach, we can calculate only the
+    // sum without storing the rounds in a vector.
+    let mut total_score = 0;
+    for round in include_str!("../input.txt").lines().map(Round::from_str) {
+        total_score += round?.my_score();
+    }
+
+    println!("total score: {}", total_score);
+
+    Ok(())
+}
+fn with_iterators() -> color_eyre::Result<()> {
+    let total_score: usize = itertools::process_results(
+        include_str!("../input.txt")
+            .lines()
+            .map(Round::from_str)
+            .map(|round| round.map(|round| round.my_score())),
+        |it| it.sum(),
+    )?;
+
+    println!("total score: {}", total_score);
+
+    Ok(())
+}
+
+fn calculate_score(opponents_play: Move, my_play: Move) -> usize {
     let shape_score = match my_play {
-        HandShape::Rock => 1,
-        HandShape::Paper => 2,
-        HandShape::Scissors => 3,
+        Move::Rock => 1,
+        Move::Paper => 2,
+        Move::Scissors => 3,
     };
 
     let outcome_score = match opponents_play {
-        HandShape::Rock => {
-            if my_play == HandShape::Rock {
+        Move::Rock => {
+            if my_play == Move::Rock {
                 3
-            } else if my_play == HandShape::Paper {
+            } else if my_play == Move::Paper {
                 6
             } else {
                 // Scissors
                 0
             }
         }
-        HandShape::Paper => {
-            if my_play == HandShape::Rock {
+        Move::Paper => {
+            if my_play == Move::Rock {
                 0
-            } else if my_play == HandShape::Paper {
+            } else if my_play == Move::Paper {
                 3
             } else {
                 // Scissors
                 6
             }
         }
-        HandShape::Scissors => {
-            if my_play == HandShape::Rock {
+        Move::Scissors => {
+            if my_play == Move::Rock {
                 6
-            } else if my_play == HandShape::Paper {
+            } else if my_play == Move::Paper {
                 0
             } else {
                 // Scissors
@@ -66,15 +188,15 @@ fn calculate_score(opponents_play: HandShape, my_play: HandShape) -> usize {
 impl Round {
     fn new_plays_from_chars(char_opponents_play: char, char_my_play: char) -> Round {
         let opponents_play = match char_opponents_play {
-            'A' => HandShape::Rock,
-            'B' => HandShape::Paper,
-            'C' => HandShape::Scissors,
+            'A' => Move::Rock,
+            'B' => Move::Paper,
+            'C' => Move::Scissors,
             _ => unreachable!(),
         };
         let my_play = match char_my_play {
-            'X' => HandShape::Rock,
-            'Y' => HandShape::Paper,
-            'Z' => HandShape::Scissors,
+            'X' => Move::Rock,
+            'Y' => Move::Paper,
+            'Z' => Move::Scissors,
             _ => unreachable!(),
         };
         Round {
@@ -85,9 +207,9 @@ impl Round {
     }
     fn new_outcomes_from_chars(char_opponents_play: char, char_outcome: char) -> Round {
         let opponents_play = match char_opponents_play {
-            'A' => HandShape::Rock,
-            'B' => HandShape::Paper,
-            'C' => HandShape::Scissors,
+            'A' => Move::Rock,
+            'B' => Move::Paper,
+            'C' => Move::Scissors,
             _ => unreachable!(),
         };
         let outcome = match char_outcome {
@@ -98,33 +220,33 @@ impl Round {
         };
         let my_play = match outcome {
             Outcome::Lose => {
-                if opponents_play == HandShape::Rock {
-                    HandShape::Scissors
-                } else if opponents_play == HandShape::Paper {
-                    HandShape::Rock
+                if opponents_play == Move::Rock {
+                    Move::Scissors
+                } else if opponents_play == Move::Paper {
+                    Move::Rock
                 } else {
                     // opponents_play == HandShape::Scissors
-                    HandShape::Paper
+                    Move::Paper
                 }
             }
             Outcome::Draw => {
-                if opponents_play == HandShape::Rock {
-                    HandShape::Rock
-                } else if opponents_play == HandShape::Paper {
-                    HandShape::Paper
+                if opponents_play == Move::Rock {
+                    Move::Rock
+                } else if opponents_play == Move::Paper {
+                    Move::Paper
                 } else {
                     // opponents_play == HandShape::Scissors
-                    HandShape::Scissors
+                    Move::Scissors
                 }
             }
             Outcome::Win => {
-                if opponents_play == HandShape::Rock {
-                    HandShape::Paper
-                } else if opponents_play == HandShape::Paper {
-                    HandShape::Scissors
+                if opponents_play == Move::Rock {
+                    Move::Paper
+                } else if opponents_play == Move::Paper {
+                    Move::Scissors
                 } else {
                     // opponents_play == HandShape::Scissors
-                    HandShape::Rock
+                    Move::Rock
                 }
             }
         };
@@ -135,7 +257,8 @@ impl Round {
         }
     }
 }
-fn main() {
+
+fn imperative() -> color_eyre::Result<()> {
     let input_filename = String::from("input.txt");
 
     let content = fs::read_to_string(&input_filename).unwrap();
@@ -155,12 +278,6 @@ fn main() {
         rounds_puzzle_2.push(Round::new_outcomes_from_chars(char_0, char_1));
     }
 
-    println!(
-        "Rounds: {} and {}",
-        rounds_puzzle_1.len(),
-        rounds_puzzle_2.len()
-    );
-
     // println!("Rounds: {:?}", rounds);
 
     let total_score_puzzle_1: usize = rounds_puzzle_1.iter().map(|r| r.score).sum();
@@ -168,4 +285,17 @@ fn main() {
 
     let total_score_puzzle_2: usize = rounds_puzzle_2.iter().map(|r| r.score).sum();
     println!("Total score for Puzzle 2: {}", total_score_puzzle_2);
+
+    Ok(())
+}
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    imperative()?;
+
+    more_rustlike()?;
+
+    with_iterators()?;
+
+    Ok(())
 }
