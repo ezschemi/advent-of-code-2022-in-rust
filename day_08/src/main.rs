55832361c7da2c082465e8be3fc84a24835fa685
Enrico Zschemisch <en@ruw.io>
2022-12-14 11:40:38 +0100

    Added imperative solution for puzzle 1 of day 7.

---
+++ day_08/src/main.rs
@@ -0,0 +1,133 @@
+use std::fs;
+
+fn is_tree_visible(heights: &Vec<Vec<u8>>, tree_x: usize, tree_y: usize) -> bool {
+    let tree_height = heights[tree_y][tree_x];
+
+    // check North direction
+    let mut max_height_north = 0;
+    for y in 0..tree_y {
+        let height = heights[y][tree_x];
+
+        if height > max_height_north {
+            max_height_north = height;
+        }
+    }
+
+    // check South direction
+    let mut max_height_south = 0;
+    for y in tree_y + 1..heights.len() {
+        let height = heights[y][tree_x];
+
+        if height > max_height_south {
+            max_height_south = height;
+        }
+    }
+
+    // check West direction
+    let row = &heights[tree_y];
+    let mut max_height_west = 0;
+    for x in 0..tree_x {
+        let height = heights[tree_y][x];
+
+        if height > max_height_west {
+            max_height_west = height;
+        }
+    }
+
+    // check East direction
+    let mut max_height_east = 0;
+    for x in tree_x + 1..row.len() {
+        let height = heights[tree_y][x];
+
+        if height > max_height_east {
+            max_height_east = height;
+        }
+    }
+
+    max_height_north < tree_height
+        || max_height_east < tree_height
+        || max_height_south < tree_height
+        || max_height_west < tree_height
+}
+
+fn convert_input_lines(lines: &Vec<&str>) -> Vec<Vec<u8>> {
+    let mut heights: Vec<Vec<u8>> = Vec::new();
+
+    for line in lines {
+        let mut this_line = Vec::new();
+        for c in line.as_bytes() {
+            let i: u8 = c - 48u8;
+
+            this_line.push(i);
+        }
+
+        heights.push(this_line);
+    }
+
+    heights
+}
+fn main() {
+    // let lines = vec!["30373", "25512", "65332", "33549", "35390"];
+
+    // let lines = include!("../input.txt");
+    let input_file_content = fs::read_to_string("input.txt").unwrap();
+    let lines = input_file_content.lines().collect();
+
+    let heights: Vec<Vec<u8>> = convert_input_lines(&lines);
+
+    println!("Lines:\n{:?}", heights);
+
+    let mut n_visible_trees = 0;
+
+    // dont check the trees on the borders of the grid, they are always visible
+    for y in 1..heights.len() - 1 {
+        let row = &heights[y];
+        for x in 1..row.len() - 1 {
+            // let height = heights[y][x];
+            let is_visible = is_tree_visible(&heights, x, y);
+
+            // println!("Tree with height {height} at ({x}, {y}) is visible: {is_visible}");
+
+            if is_visible {
+                n_visible_trees += 1;
+            }
+        }
+    }
+
+    // add the number of trees that are visible on the borders (all of them)
+    let n_border_trees_visible =
+        heights[0].len() + heights[0].len() + heights.len() - 2 + heights.len() - 2;
+    dbg!(n_visible_trees);
+    dbg!(n_border_trees_visible);
+    n_visible_trees += n_border_trees_visible;
+
+    let n_trees = heights.len() * heights[0].len();
+
+    println!("Out of the {n_trees} trees, {n_visible_trees} are visible.");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::convert_input_lines;
+    use crate::is_tree_visible;
+    use test_case::test_case;
+
+    #[test_case(1, 1, true)]
+    #[test_case(2, 1, true)]
+    #[test_case(3, 1, false)]
+    #[test_case(1, 2, true)]
+    #[test_case(2, 2, false)]
+    #[test_case(3, 2, true)]
+    #[test_case(1, 3, false)]
+    #[test_case(2, 3, true)]
+    #[test_case(3, 3, false)]
+    fn test_with_sample_input(tree_x: usize, tree_y: usize, should_be_visible: bool) {
+        let lines = vec!["30373", "25512", "65332", "33549", "35390"];
+
+        let heights: Vec<Vec<u8>> = convert_input_lines(&lines);
+
+        let is_visible = is_tree_visible(&heights, tree_x, tree_y);
+
+        assert_eq!(should_be_visible, is_visible);
+    }
+}
